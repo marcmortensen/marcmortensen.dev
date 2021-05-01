3cf8a11df056a925dce6d5bf312db3d9dc9b820a
Marc <marc@tekla.io>
2021-05-01 16:56:16 +0200

    Adding new tecnologies

--- utils/technology.js
+++ utils/technology.js
@@ -2,8 +2,16 @@ import BrainJs from '@/components/Icon/BrainJs/index.vue';
 import NodeJs from '@/components/Icon/NodeJs/index.vue';
 import TypeScript from '@/components/Icon/TypeScript/index.vue';
 import Yarn from '@/components/Icon/Yarn/index.vue';
+import Npm from '@/components/Icon/Npm/index.vue';
+import Jest from '@/components/Icon/Jest/index.vue';
+import Github from '@/components/Icon/Github/index.vue';
+import Fiji from '@/components/Icon/Fiji/index.vue';
 
 export const brainJs = { icon: BrainJs, name: 'BrainJs' };
 export const nodeJs = { icon: NodeJs, name: 'NodeJs' };
 export const typeScript = { icon: TypeScript, name: 'TypeScript' };
 export const yarn = { icon: Yarn, name: 'Yarn' };
+export const npm = { icon: Npm, name: 'Npm' };
+export const jest = { icon: Jest, name: 'Jest' };
+export const github = { icon: Github, name: 'Github' };
+export const fiji = { icon: Fiji, name: 'Fiji' };
